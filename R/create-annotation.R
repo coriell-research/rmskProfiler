bf8fbb7d86079ad21d8d1b3966146607b9276442
jcalendo <jcalendo@pm.me>
2024-08-25 13:14:28 -0400

    Version of createAnnotation that does not rely on annotatr and uses downloaded GTF for annotations

--- R/create-annotation.R
+++ R/create-annotation.R
@@ -1,41 +1,12 @@
-#' Generate annotation resources for annotating TE loci and genes
+#' Extract duplicate info from json file as a data.table
 #'
-#' This function produces multiple annotation files used to annotate downstream
-#' counts generated after quantification with Salmon. It creates: GRangesList
-#' object for each Hash value and all unique ranges for the hash and a
-#' tab-delimited file of unique hash-by-overlap information. This function
-#' generates overlap annotations with respect to the annotations provided by
-#' annotatr::build_annotations() on either hg38_basicgenes or mm10_basicgenes,
-#' depending on the resources detected.
-#'
-#' @param resource_dir Path to the directory containing index generation resources.
-#' Output is saved to this location.
-#' @param stranded Should the annotation be created with respect to strand? Default
-#' TRUE. If stranded=FALSE then TE overlap annotation is performed on sequence
-#' positions without respect to strand.
-#'
-#' @return NULL
+#' @param jsonfile JSON file containing information about rmsk sequences
 #' @import data.table
-#' @export
+#' @return data.table
 #'
-#' @examples
-#' \dontrun{
-#' createAnnotation(resource_dir = "/path/to/rmsk-resources")
-#' }
-createAnnotation <- function(resource_dir, stranded = TRUE) {
-
-  resources <- list.files(resource_dir, full.names = TRUE)
-  info_json <- grep("rmsk-duplicateInfo.json", resources, value = TRUE)
-  genome_fa <- grep("primary_assembly.genome.fa.gz", resources, value = TRUE)
-  build <- "hg38"
-  if (grepl("GRCm38", genome_fa)) {
-    build <- "mm10"
-  }
-
-  # Process duplicate info --------------------------------------------------
-
-  message("Reading in the duplicate information from ", basename(info_json), "...")
-  info <- jsonlite::fromJSON(info_json)
+.dupInfoToDT <- function(jsonfile) {
+  message("Reading in the duplicate information from ", basename(jsonfile), "...")
+  info <- jsonlite::fromJSON(jsonfile)
 
   message("Creating data.table from json information...")
   dt <- data.table::data.table(Hash = names(info), Instance = info)
@@ -52,66 +23,107 @@ createAnnotation <- function(resource_dir, stranded = TRUE) {
 
   message("Extracting position information...")
   dt[, c("seqnames", "position") := data.table::tstrsplit(Location, ":", fixed = TRUE)]
-
   dt[, strand := data.table::fifelse(stringi::stri_detect(position, regex = "\\(+\\)"), "+", "-")]
-
   dt[, position := stringi::stri_replace(position, "", regex = "\\(.\\)")][,
        c("start", "end") := data.table::tstrsplit(position, "-", fixed = TRUE)][,
        `:=`(start = as.integer(start), end = as.integer(end))]
-
   dt[, `:=`(Location = NULL, position = NULL, Instance = NULL, ID = NULL)]
 
-  if (isTRUE(stranded)) {
-    data.table::setkey(dt, seqnames, strand, start, end)
-  } else {
-    data.table::setkey(dt, seqnames, start, end)
-  }
+  return(dt)
+}
+
+
+#' Find overlaps of TE loci regions with genomic features
+#'
+#' Finds any TE that overlaps with an exon by transcript, and intron by
+#' transcript, a 3 prime UTR by transcript, a 5 prime UTR by transcript, or
+#' a promoter by gene.
+#'
+#' @param gtffile Path to GTF file to create annotation from
+#' @param x A GRanges object of TE loci
+#' @param stranded TRUE/FALSE should overlaps be computed with respect to strand
+#'
+#' @return List of hash vectors overlapping genomic features
+#'
+.getHashOverlaps <- function(x, ignore, gtffile) {
+  message("Creating TxDb from GTF...")
+  txdb <- suppressWarnings(txdbmaker::makeTxDbFromGFF(gtffile))
+
+  message("Extracting genomic regions from txdb...")
+  exons_by_tx <- unlist(GenomicFeatures::exonsBy(txdb, by = "tx"))
+  introns_by_tx <- unlist(GenomicFeatures::intronsByTranscript(txdb))
+  promoters_by_gene <- GenomicRanges::promoters(GenomicFeatures::genes(txdb))
+  threeUTR_by_tx <- unlist(GenomicFeatures::threeUTRsByTranscript(txdb))
+  fiveUTR_by_tx <- unlist(GenomicFeatures::fiveUTRsByTranscript(txdb))
+
+  message("Finding overlaps between TE loci and genomic features...")
+  exon_hits <- GenomicRanges::findOverlaps(x, exons_by_tx, ignore.strand = !ignore)
+  intron_hits <- GenomicRanges::findOverlaps(x, introns_by_tx, ignore.strand = !ignore)
+  promoter_hits <- GenomicRanges::findOverlaps(x, promoters_by_gene, ignore.strand = !ignore)
+  threeUTR_hits <- GenomicRanges::findOverlaps(x, threeUTR_by_tx, ignore.strand = !ignore)
+  fiveUTR_hits <- GenomicRanges::findOverlaps(x, fiveUTR_by_tx, ignore.strand = !ignore)
+
+  message("Collecting results...")
+  hash_in_exon <- unique(x[S4Vectors::queryHits(exon_hits), ]$Hash)
+  hash_in_intron <- unique(x[S4Vectors::queryHits(intron_hits), ]$Hash)
+  hash_in_promoter <- unique(x[S4Vectors::queryHits(promoter_hits), ]$Hash)
+  hash_in_3utr <- unique(x[S4Vectors::queryHits(threeUTR_hits), ]$Hash)
+  hash_in_5utr <- unique(x[S4Vectors::queryHits(fiveUTR_hits), ]$Hash)
+
+  result <- list(
+     hash_in_exon = hash_in_exon,
+     hash_in_intron = hash_in_intron,
+     hash_in_promoter = hash_in_promoter,
+     hash_in_3utr = hash_in_3utr,
+     hash_in_5utr = hash_in_5utr
+    )
+
+  return(result)
+}
+
+
+#' Generate annotation resources for annotating TE loci and genes
+#'
+#' This function produces multiple annotation files used to annotate downstream
+#' counts generated after quantification with Salmon. It creates a GRangesList
+#' object for each hash and a tab-delimited file containing annotation
+#' information for each hash with indicator values for the types of genic
+#' features they overlap. Overlap annotations are generated with respect to the
+#' downloaded gencode GTF file.
+#'
+#' @param resource_dir Path to the directory containing index generation resources.
+#' Output is saved to this location.
+#' @param stranded Should the annotation be created with respect to strand? Default
+#' TRUE. If stranded=FALSE then TE overlap annotation is performed without respect
+#' to strand, i.e. findOverlaps(..., ignore.strand = TRUE).
+#'
+#' @return NULL
+#' @import data.table
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' createAnnotation(resource_dir = "/path/to/rmsk-resources")
+#' }
+createAnnotation <- function(resource_dir, stranded = TRUE) {
+
+  resources <- list.files(resource_dir, full.names = TRUE)
+  info_json <- grep("rmsk-duplicateInfo.json", resources, value = TRUE)
+  gtf_file <- grep("annotation.gtf.gz", resources, value = TRUE)
+
+  dt <- .dupInfoToDT(info_json)
 
   message("Creating a GRangesList for all TE ranges...")
   gr <- GenomicRanges::makeGRangesFromDataFrame(dt, keep.extra.columns = TRUE)
   grl <- S4Vectors::splitAsList(gr, gr$Hash)
 
-  # Create annotations ------------------------------------------------------
-
-  message("Building genic annotation with `annotatr`...")
-  if (build == "hg38") {
-    anno <- suppressMessages(annotatr::build_annotations("hg38", "hg38_basicgenes"))
-  } else if (build == "mm10") {
-    anno <- suppressMessages(annotatr::build_annotations("mm10", "mm10_basicgenes"))
-  } else {
-    stop("build must be one of 'hg38' or 'mm10'")
-  }
-
-  # Coerce to data.table, key, and remove non-standard chromosomes
-  anno <- data.table::as.data.table(data.frame(anno))
-
-  if (isTRUE(stranded)) {
-    data.table::setkey(anno, seqnames, strand, start, end)
-  } else {
-    data.table::setkey(anno, seqnames, start, end)
-  }
-
-  keep_chrom <- paste("chr", c(1:22, "X", "Y"), sep = "")
-  anno <- anno[seqnames %in% keep_chrom]
-
-  # Perform overlap ---------------------------------------------------------
-
-  message("Overlapping TE locations with genic annotations...")
-  ov <- data.table::foverlaps(dt, anno, type = "any")
-
-  # If no overlaps with genic regions then type is intergenic
-  ov[is.na(type), `:=`(
-    type = stringi::stri_c(build, "_genes_intergenic"),
-    start = 0L, end = 0L, width = 0L
-  )]
-
-  # Rowdata -----------------------------------------------------------------
+  ov <- .getHashOverlaps(gr, stranded, gtf_file)
 
   message("Getting all unique hash-element pairs...")
   hash_dt <- dt[, .(N_Loci = .N), by = .(Hash, RepName)]
   hash_dt[, c("Class", "Family", "Subfamily") := data.table::tstrsplit(RepName, ".", fixed = TRUE)]
 
-  message("Collapsing hash-level information into rowData (this may take some time)...")
+  message("Collapsing hash-level information into rowData...")
   by_hash <- hash_dt[, .(
     Class = stringi::stri_flatten(unique(Class), collapse = ","),
     Family = stringi::stri_flatten(unique(Family), collapse = ","),
@@ -120,34 +132,21 @@ createAnnotation <- function(resource_dir, stranded = TRUE) {
     N_Class = length(unique(Class)),
     N_Family = length(unique(Family)),
     N_Subfamily = length(unique(Subfamily))
-  ),
-  by = Hash
-  ]
-
-  message("Annotating hashes with genomic location...")
-  is_intergenic <- ov[stringi::stri_detect(type, fixed = "intergenic"), unique(Hash)]
-  is_1to5kb <- ov[stringi::stri_detect(type, fixed = "1to5kb"), unique(Hash)]
-  is_promoter <- ov[stringi::stri_detect(type, fixed = "promoters"), unique(Hash)]
-  is_exonic <- ov[stringi::stri_detect(type, fixed = "exons"), unique(Hash)]
-  is_intronic <- ov[stringi::stri_detect(type, fixed = "introns"), unique(Hash)]
-  is_3utr <- ov[stringi::stri_detect(type, fixed = "3UTRs"), unique(Hash)]
-  is_5utr <- ov[stringi::stri_detect(type, fixed = "5UTRs"), unique(Hash)]
+  ), by = Hash]
 
   by_hash[, `:=`(
-    hasIntergenic = Hash %chin% is_intergenic,
-    has1to5kb = Hash %chin% is_1to5kb,
-    hasPromoter = Hash %chin% is_promoter,
-    hasExonic = Hash %chin% is_exonic,
-    hasIntronic = Hash %chin% is_intronic,
-    has3UTR = Hash %chin% is_3utr,
-    has5UTR = Hash %chin% is_5utr
-  )]
-
-  message("Writing TE GRangesList to: ", file.path(resource_dir, "rmsk-grl.rds"))
-  saveRDS(grl, file.path(resource_dir, "rmsk-grl.rds"))
+    hasPromoter = Hash %chin% ov$hash_in_promoter,
+    hasExonic = Hash %chin% ov$hash_in_exon,
+    hasIntronic = Hash %chin% ov$hash_in_intron,
+    has3UTR = Hash %chin% ov$hash_in_3utr,
+    has5UTR = Hash %chin% ov$hash_in_5utr)][,
+    hasIntergenic := (!hasExonic & !hasIntronic & !has3UTR & !has5UTR)]
 
   message("Writing out hash-level rowData to: ", file.path(resource_dir, "rmsk-rowData.tsv"))
   data.table::fwrite(by_hash, file.path(resource_dir, "rmsk-rowData.tsv"), sep = "\t")
+
+  message("Writing TE GRangesList to: ", file.path(resource_dir, "rmsk-grl.rds"))
+  saveRDS(grl, file.path(resource_dir, "rmsk-grl.rds"))
   message("Done.")
 
   return(invisible(NULL))
