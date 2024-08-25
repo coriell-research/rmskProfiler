4c05b287883129d3098ea5dbd7cfe27cbf35dd66
jcalendo <jcalendo@pm.me>
2024-08-25 14:37:56 -0400

    Delay pybedtools import on load

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,8 @@
 rmsk_profiler <- NULL
+pybedtools <- NULL
 
 .onLoad <- function(libname, pkgname) {
   path <- system.file("python", package = "rmskProfiler")
   rmsk_profiler <<- reticulate::import_from_path("rmsk_profiler", path = path, delay_load = TRUE)
+  pybedtools <<- reticulate::import_from_path("pybedtools", path = path, delay_load = TRUE)
 }
